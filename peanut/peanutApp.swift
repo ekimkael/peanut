930bb10eb368e514fa4b5c8e38b9e793f99bb249
Ekim Kael <michelmbili@gmail.com>
2021-09-28 20:38:56 +0100

    Initial Commit

---
+++ peanut/peanutApp.swift
@@ -0,0 +1,17 @@
+//
+//  peanutApp.swift
+//  peanut
+//
+//  Created by Ekim Kael on 28/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct peanutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
